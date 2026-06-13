31d7e3948a6806a08ed64187dbabd9e66e756397
Palak Mani Tripathi <tripathipalakk25@gmail.com>
2026-06-13 20:57:28 +0530

    Update category page details API

--- controllers/Category.js
+++ controllers/Category.js
@@ -57,3 +57,51 @@ exports.showAllCategory = async (req, res) => {
         });
     }
 };
+
+
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+
+        //get categoryId
+        const {categoryId} = req.body;
+
+        //get courses for specified categoryId
+        const selectedCategory = await Category.findById(categoryId)
+            .populate("courses")
+            .exec();
+
+        //validation
+        if(!selectedCategory) {
+            return res.status(404).json({
+                success:false,
+                message:"Data Not Found",
+            });
+        }
+
+        //get courses for different categories
+        const differentCategories = await Category.find({
+            _id: {$ne: categoryId},
+        })
+        .populate("courses")
+        .exec();
+
+        //get top selling courses.......................
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            data: {
+                selectedCategory,
+                differentCategories,
+            },
+        });
+
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
