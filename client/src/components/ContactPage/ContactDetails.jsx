ec535cf328b7ab1c3ff952936a50dac79f88180a
Palak Mani Tripathi <tripathipalakk25@gmail.com>
2026-07-06 22:40:26 +0530

    Update contact page form and details

--- client/src/components/ContactPage/ContactDetails.jsx
+++ client/src/components/ContactPage/ContactDetails.jsx
@@ -8,20 +8,20 @@ const contactDetails = [
     icon: "HiChatBubbleLeftRight",
     heading: "Chat on us",
     description: "Our friendly team is here to help.",
-    details: "info@studynotion.com",
+    details: "info@skillrise.com",
   },
   {
     icon: "BiWorld",
     heading: "Visit us",
     description: "Come and say hello at our office HQ.",
     details:
-      "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+      "Lucknow, Uttar Pradesh, India",
   },
   {
     icon: "IoCall",
     heading: "Call us",
     description: "Mon - Fri From 8am to 5pm",
-    details: "+123 456 7869",
+    details: "+91 1234567890",
   },
 ];
 
