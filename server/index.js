bf83a7ec2ca4d669982c0592d2f53b5ad8321671
Palak Mani Tripathi <tripathipalakk25@gmail.com>
2026-08-02 00:30:12 +0530

    Fix backend URL for production

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://skill-rise-orcin.vercel.app",
         credentials: true,
     })
 )
